0c02298500592328ec089f7a37ed0d189d765acc
Mefodei <mefodei.link@gmail.com>
2025-04-10 05:12:47 +0700

    - update ads composite service

--- CompositeProvider/CompositeAdsService.cs
+++ CompositeProvider/CompositeAdsService.cs
@@ -2,50 +2,67 @@ namespace VN.Game.Modules.unigame.levelplay.AdsCommonProvider
 {
     using System;
     using System.Collections.Generic;
+    using System.Linq;
     using System.Threading.Tasks;
     using Cysharp.Threading.Tasks;
     using global::Game.Modules.unigame.levelplay.Shared;
     using global::Game.Runtime.Game.Liveplay.Ads.Runtime;
     using UniGame.Core.Runtime;
     using UniModules.UniCore.Runtime.DataFlow;
+    using UniRx;
 
     public class CompositeAdsService : IAdsService
     {
         private LifeTimeDefinition _lifeTime;
-        public ILifeTime LifeTime => _lifeTime;
-        public bool RewardedAvailable { get; }
-        public bool InterstitialAvailable { get; }
-        public IObservable<AdsActionData> AdsAction { get; }
-        private List<IAdsService> adsServices;
-        private IAdsService serviceWithAvaiableAds = null;
+        private List<IAdsService> _adsServices;
+        private IAdsService _serviceWithAvailableAds;
+        private Subject<AdsActionData> _adsAction;
+        
         public CompositeAdsService(List<IAdsService> services)
         {
-            adsServices = services;
+            _adsServices = services;
             _lifeTime = new LifeTimeDefinition();
+            _adsAction = new Subject<AdsActionData>()
+                .AddTo(_lifeTime);
+            
+            foreach (var service in _adsServices)
+                service.AdsAction.Subscribe(_adsAction).AddTo(_lifeTime);
         }
-        public void Dispose()
-        {
-            _lifeTime.Terminate();
-        }
+        
+        public ILifeTime LifeTime => _lifeTime;
+
+        public bool RewardedAvailable => _adsServices.Any(x => x.RewardedAvailable);
+        
+        public bool InterstitialAvailable  => _adsServices.Any(x => x.InterstitialAvailable);
+        public IObservable<AdsActionData> AdsAction => _adsAction;
+        
+        public void Dispose() => _lifeTime.Terminate();
 
         public void ValidateIntegration()
         {
-            throw new NotImplementedException();
+            foreach (var adsService in _adsServices)
+            {
+                adsService.ValidateIntegration();
+            }
         }
 
         public bool IsPlacementAvailable(string placementName)
         {
-            foreach (var service in adsServices)
+            foreach (var service in _adsServices)
             {
-                if (service.IsPlacementAvailable(placementName))
-                {
-                    serviceWithAvaiableAds = service;
-                    return true;
-                }
+                if (!service.IsPlacementAvailable(placementName)) continue;
+                _serviceWithAvailableAds = service;
+                return true;
             }
 
             return false;
         }
+        
+        public async UniTask<AdsShowResult> ShowRewardedAdAsync(string placeId)
+        {
+            return await _serviceWithAvailableAds.ShowRewardedAdAsync(placeId);
+        }
+        
         public UniTask LoadAdsAsync()
         {
             throw new NotImplementedException();
@@ -64,11 +81,6 @@ namespace VN.Game.Modules.unigame.levelplay.AdsCommonProvider
         public UniTask<AdsShowResult> Show(PlacementType type)
         {
             throw new NotImplementedException();
-        }
-
-        public async UniTask<AdsShowResult> ShowRewardedAdAsync(string placeId)
-        {
-            return await serviceWithAvaiableAds.ShowRewardedAdAsync(placeId);
         }
 
         public UniTask<AdsShowResult> ShowInterstitialAdAsync(string placeId)
